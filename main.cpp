88ecc0c01f88fe7253d6adc94835ca4a2ef70d77
Quamiemma <42769719+Quamiemma@users.noreply.github.com>
2018-10-15 17:28:40 +0000

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+strict{
+string idNum;
+string name;
+int age;
+string gender;
+double mark;
+char grade;
+};
+
+int main () {
+
+fstream get
+
+return 0;
+
+} 
